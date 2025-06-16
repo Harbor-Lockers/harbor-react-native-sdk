0e184d58e6157988d9c37a83d5e82c1c19239bcb
Gerardo Montemayor-García <gerardogarcia@theluxergroup.com>
2025-06-16 13:36:50 -0600

    New Architecture Initial Commit

---
+++ ios/ReactNativeSdk.h
@@ -0,0 +1,5 @@
+#import <ReactNativeSdkSpec/ReactNativeSdkSpec.h>
+
+@interface ReactNativeSdk : NSObject <NativeReactNativeSdkSpec>
+
+@end
---
+++ src/index.tsx
@@ -0,0 +1,5 @@
+import ReactNativeSdk from './NativeReactNativeSdk';
+
+export function multiply(a: number, b: number): number {
+  return ReactNativeSdk.multiply(a, b);
+}
